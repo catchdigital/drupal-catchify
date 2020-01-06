ac7a3a75da1922b5ad5719c19bf81c5f8c2e6f4d
Arthur battut <a.battut@catchdigital.com>
2020-01-06 16:06:10 +0000

    Add materialize

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 // Please note that jQuery is a global dependency
+import M from 'materialize-css';
 import print from './components/print';
 
 window.$ = window.jQuery;
 
+M.AutoInit();
 print($);
 
 window.Drupal.behaviors = {
