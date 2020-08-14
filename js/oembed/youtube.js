1ff60f4c43c2f45906120b31f33d6a816818ac0e
Alberto Contreras <a.contreras@catchdigital.com>
2020-08-14 09:38:13 +0100

    #7: Add oEmbed integration scripts.

---
+++ js/oembed/youtube.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Behaviors to implement control over youtube video tag.
+ */
+
+'use strict';
+window.video = {
+  video: null,
+
+  init: function() {
+    var iframe = document.querySelector('iframe');
+
+    // Enable js api
+    var domain = window.location.origin;
+    var youtubeURL = String(iframe.src);
+    youtubeURL = youtubeURL + "&enablejsapi=1";
+    youtubeURL = youtubeURL + `&origin=${domain}`;
+    iframe.src = youtubeURL;
+
+    // Create video instance
+    this.video = new YT.Player(iframe, {});
+  },
+
+  pause: function () {
+    if (!this.video) {
+      this.init();
+    }
+    this.video.pauseVideo();
+  },
+
+  play: function () {
+    if (!this.video) {
+      this.init();
+    }
+    this.video.playVideo();
+  },
+};
+
+window.onYouTubePlayerAPIReady = function() {
+  window.video.init();
+}
