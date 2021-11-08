7401e300e3d1ec03b812d82c9c9eca268bad5381
Jens <j.janson@catchdigital.com>
2021-11-08 08:34:25 +0000

    12: Removed arrow functions for IE11 comp

--- js/oembed/youtube.js
+++ js/oembed/youtube.js
@@ -3,69 +3,72 @@
  * Behaviors to implement control over youtube video tag.
  */
 
-'use strict';
-window.video = {
-  video: null,
-
-  init: function () {
-    var iframe = document.querySelector('iframe');
-
-    // Enable js api
-    var domain = window.location.origin;
-    var youtubeURL = String(iframe.src);
-    youtubeURL = youtubeURL + '&enablejsapi=1';
-    youtubeURL = youtubeURL + `&origin=${domain}`;
-    iframe.src = youtubeURL;
-
-    // Create video instance
-    if (YT.Player) {
-      this.video = new YT.Player(iframe, {});
-    }
-  },
-
-  pause: function () {
-    if (!this.video) {
-      this.init();
-    }
-    this.poll(() => {
-      try {
-        this.video.pauseVideo();
-        return true;
-      } catch {
-        return false;
-      }
-    });
-  },
-
-  play: function () {
-    if (!this.video) {
-      this.init();
-    }
-    this.poll(() => {
-      try {
-        this.video.playVideo();
-        return true;
-      } catch {
-        return false;
-      }
-    });
-  },
-
-  poll: function (fn) {
-    var attempts = 0;
-    var maxAttempts = 25;
-    var interval = 100;
-    (function p() {
-      if (fn()) {
-        return;
-      } else if (attempts < maxAttempts) {
-        attempts++;
-        setTimeout(p, interval);
-      }
-    })();
-  }
-};
-
-window.onYouTubePlayerAPIReady = function () {
-  window.video.init();
-};
+ 'use strict';
+ window.video = {
+   video: null,
+ 
+   init: function () {
+     var iframe = document.querySelector('iframe');
+ 
+     // Enable js api
+     var domain = window.location.origin;
+     var youtubeURL = String(iframe.src);
+     youtubeURL = youtubeURL + '&enablejsapi=1';
+     youtubeURL = youtubeURL + `&origin=${domain}`;
+     iframe.src = youtubeURL;
+ 
+     // Create video instance
+     if (YT.Player) {
+       this.video = new YT.Player(iframe, {});
+     }
+   },
+ 
+   pause: function () {
+     if (!this.video) {
+       this.init();
+     }
+     var _this = this;
+     this.poll(function () {
+       try {
+         _this.video.pauseVideo();
+         return true;
+       } catch {
+         return false;
+       }
+     });
+   },
+ 
+   play: function () {
+     if (!this.video) {
+       this.init();
+     }
+     var _this = this;
+     this.poll(function () {
+       try {
+         _this.video.playVideo();
+         return true;
+       } catch {
+         return false;
+       }
+     });
+   },
+ 
+   poll: function (fn) {
+     var attempts = 0;
+     var maxAttempts = 25;
+     var interval = 100;
+     (function p() {
+       if (fn()) {
+         return;
+       } else if (attempts < maxAttempts) {
+         attempts++;
+         setTimeout(p, interval);
+       }
+     })();
+   }
+ };
+ 
+ window.onYouTubePlayerAPIReady = function () {
+   window.video.init();
+ };
+ 
