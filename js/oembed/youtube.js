6b4f035120ec4e2153557f2e0a1d4c1418b31879
George Phillips <g.phillips@catchdigital.com>
2024-05-08 09:56:50 +0100

    Added init to the play and pause functions just incase.

--- js/oembed/youtube.js
+++ js/oembed/youtube.js
@@ -3,65 +3,71 @@
  * Behaviors to implement control over youtube video tag.
  */
 
- 'use strict';
- window.video = {
-   video: null,
+'use strict';
+window.video = {
+  video: null,
 
-   init: function () {
-     var iframe = document.querySelector('iframe');
+  init: function () {
+    var iframe = document.querySelector('iframe');
 
-     // Enable js api
-     var domain = window.location.origin;
-     var youtubeURL = String(iframe.src);
-     youtubeURL = youtubeURL + '&enablejsapi=1';
-     youtubeURL = youtubeURL + `&origin=${domain}`;
-     iframe.src = youtubeURL;
+    // Enable js api
+    var domain = window.location.origin;
+    var youtubeURL = String(iframe.src);
+    youtubeURL = youtubeURL + '&enablejsapi=1';
+    youtubeURL = youtubeURL + `&origin=${domain}`;
+    iframe.src = youtubeURL;
 
-     // Create video instance
-     if (YT.Player) {
-       this.video = new YT.Player(iframe, {});
-     }
-   },
+    // Create video instance
+    if (YT.Player) {
+      this.video = new YT.Player(iframe, {});
+    }
+  },
 
-   pause: function () {
-     var _this = this;
-     this.poll(function () {
-       try {
-         _this.video.pauseVideo();
-         return true;
-       } catch {
-         return false;
-       }
-     });
-   },
+  pause: function () {
+    var _this = this;
+    if (!this.video) {
+      this.init();
+    }
+    this.poll(function () {
+      try {
+        _this.video.pauseVideo();
+        return true;
+      } catch {
+        return false;
+      }
+    });
+  },
 
-   play: function () {
-     var _this = this;
-     this.poll(function () {
-       try {
-         _this.video.playVideo();
-         return true;
-       } catch {
-         return false;
-       }
-     });
-   },
+  play: function () {
+    var _this = this;
+    if (!this.video) {
+      this.init();
+    }
+    this.poll(function () {
+      try {
+        _this.video.playVideo();
+        return true;
+      } catch {
+        return false;
+      }
+    });
+  },
 
-   poll: function (fn) {
-     var attempts = 0;
-     var maxAttempts = 25;
-     var interval = 100;
-     (function p() {
-       if (fn()) {
-         return;
-       } else if (attempts < maxAttempts) {
-         attempts++;
-         setTimeout(p, interval);
-       }
-     })();
-   }
- };
+  poll: function (fn) {
+    var attempts = 0;
+    var maxAttempts = 25;
+    var interval = 100;
+    (function p() {
+      if (fn()) {
+        return;
+      } else if (attempts < maxAttempts) {
+        attempts++;
+        setTimeout(p, interval);
+      }
+    })();
+  }
+};
 
- window.onYouTubePlayerAPIReady = function () {
-   window.video.init();
- };
+window.onYouTubePlayerAPIReady = function () {
+  window.video.init();
+};
